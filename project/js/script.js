de79c2ed5e4f1ac6dcaf822f611657c2d4c5d7b0
petitlatte <lisasumida@gmail.com>
2019-08-23 16:55:16 +1200

    added mapbox

--- project/js/script.js
+++ project/js/script.js
@@ -1,1 +1,7 @@
-console.log ("hello world!");
+mapboxgl.accessToken = 'pk.eyJ1IjoicGV0aXRsYXR0ZSIsImEiOiJjamFib2pldjcwMjJ6MnhtaHlvcXRrbmdmIn0.0MVivy-vilcBL98R80FTiw';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+  center: [173, -41],
+  zoom: 4.8,
+});
