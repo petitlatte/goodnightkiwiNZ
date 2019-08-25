a95d1b76a8fb8386e362f1d124811d15a3d9af12
petitlatte <lisasumida@gmail.com>
2019-08-25 22:32:22 +1200

    updated styles

---
+++ App/js/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicGV0aXRsYXR0ZSIsImEiOiJjamFib2pldjcwMjJ6MnhtaHlvcXRrbmdmIn0.0MVivy-vilcBL98R80FTiw';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+  center: [173, -41],
+  zoom: 4.8,
+});
--- App/js/script.js
+++ App/js/script.js
@@ -1,7 +1,1 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoicGV0aXRsYXR0ZSIsImEiOiJjamFib2pldjcwMjJ6MnhtaHlvcXRrbmdmIn0.0MVivy-vilcBL98R80FTiw';
-var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11',
-  center: [173, -41],
-  zoom: 4.8,
-});
+// alert("javascript is working!");
