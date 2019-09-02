0d086ae474120dfc131cc396093a0d58fa309242
petitlatte <lisasumida@gmail.com>
2019-09-02 20:48:41 +1200

    basic data structure

---
+++ App/js/data.js
@@ -0,0 +1,39 @@
+var data = [
+{	
+	AccomName:'A 3 star Hotel',
+	AccomType: 'Hotel'
+	maxGuest:2,
+	image:'img/1.jpg',
+	nightPrice:'$157 / night',
+	nightMulti:157,
+},
+
+{	
+
+	AccomName:'A 3 star Hotel',
+	AccomType: 'Hostel'
+	maxGuest:6,
+	image:'img/2.jpg',
+	nightPrice:'$30 / night',
+	nightMulti:30,
+},
+
+{	
+	AccomName:'A 3 star Hotel',
+	AccomType: 'Motel'
+	maxGuest:4,
+	image:'img/3.jpg',
+	nightPrice:'$90 / night',
+	nightMulti:90,
+},
+
+{
+	AccomName:'Big House',
+	AccomType: 'House'
+	maxGuest:4,
+	image:'img/4.jpg',
+	nightPrice:'$240 / night',
+	nightMulti:240,
+}
+
+];
